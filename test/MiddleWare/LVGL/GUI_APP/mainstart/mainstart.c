c196ef52cf056a6f408b785bafdb2b5e9bc65648
lilinng <2464532129@qq.com>
2026-02-27 16:34:58 +0800

    标签使用

--- test/MiddleWare/LVGL/GUI_APP/mainstart/mainstart.c
+++ test/MiddleWare/LVGL/GUI_APP/mainstart/mainstart.c
@@ -1,7 +1,7 @@
 /*
  * @Author: lilinng 2464532129@qq.com
  * @Date: 2026-02-27 11:00:26
- * @LastEditTime: 2026-02-27 13:35:51
+ * @LastEditTime: 2026-02-27 16:34:08
  * @FilePath: \test_EIDEd:\MCU\stm32\stm32_practise\VS+HAL\stm32_HAL-FreeRTOS-LVGL-\test\MiddleWare\LVGL\GUI_APP\mainstart\mainstart.c
  * @Description: 
  */
@@ -16,19 +16,28 @@ void mainstart(void)
     main_screen_width = lv_obj_get_width(lv_scr_act());
     main_screen1_height = lv_obj_get_height(lv_scr_act());
 
-    static lv_obj_t* obj1 = NULL;
-    obj1 = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(obj1,main_screen_width*2/3,main_screen1_height*2/3);
-    lv_obj_align(obj1,LV_ALIGN_CENTER,0,0);
-    lv_obj_set_style_bg_color(obj1,lv_color_hex(0x1f6feb),LV_STATE_DEFAULT);
-    lv_obj_set_style_shadow_width(obj1,(lv_coord_t)30,LV_STATE_DEFAULT);
+    static lv_obj_t* label1_obj = NULL; 
+    label1_obj = lv_label_create(lv_scr_act());
+    //设置大小和展示方式
+    lv_obj_set_size(label1_obj,50,15);
+    lv_label_set_long_mode(label1_obj,LV_LABEL_LONG_SCROLL_CIRCULAR);
+    //对齐
+    lv_obj_align(label1_obj,LV_ALIGN_CENTER,0,-30);
+    //文本
+    lv_label_set_text(label1_obj,"Welcome to LVLG");
+    //背景不透明度和颜色设置
+    lv_obj_set_style_bg_opa(label1_obj,255,LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(label1_obj,lv_color_hex(0xd2a8ff),LV_STATE_DEFAULT);
+    
+    //为某些字体单独着色
+    lv_label_set_recolor(label1_obj,true);
+    lv_label_set_text(label1_obj,"Welcome to #24acf2 LVGL#");
 
-    lv_coord_t obj1_width = lv_obj_get_width(lv_scr_act());
-    lv_coord_t obj1_height = lv_obj_get_height(lv_scr_act());
+    static lv_obj_t* User_info_label_obj = NULL;
+    User_info_label_obj = lv_label_create(lv_scr_act());
+    
+    lv_obj_set_align(User_info_label_obj,LV_ALIGN_CENTER);
 
-    static lv_obj_t* obj2 = NULL;
-    obj2 = lv_obj_create(obj1);
-    lv_obj_set_size(obj2,obj1_width*1/4,obj1_height*1/5);
-    lv_obj_align(obj2,LV_ALIGN_BOTTOM_MID,0,50);
-    lv_obj_set_style_bg_color(obj2,lv_color_hex(0xd099fb),LV_STATE_DEFAULT);
+    lv_label_set_recolor(User_info_label_obj,true);
+    lv_label_set_text(User_info_label_obj,"#b01215 User:# #19cc23 lilinng#");
 }
