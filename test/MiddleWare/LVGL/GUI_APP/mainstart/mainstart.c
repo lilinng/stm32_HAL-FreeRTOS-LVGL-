bb4ebc32a9269943528604c89d2e3fa9a9d15724
lilinng <2464532129@qq.com>
2026-03-02 17:27:41 +0800

    timer_cb,label,bar的综合应用

--- test/MiddleWare/LVGL/GUI_APP/mainstart/mainstart.c
+++ test/MiddleWare/LVGL/GUI_APP/mainstart/mainstart.c
@@ -1,7 +1,7 @@
 /*
  * @Author: lilinng 2464532129@qq.com
  * @Date: 2026-02-27 11:00:26
- * @LastEditTime: 2026-03-01 15:51:52
+ * @LastEditTime: 2026-03-02 17:26:34
  * @FilePath: \test_EIDEd:\MCU\stm32\stm32_practise\VS+HAL\stm32_HAL-FreeRTOS-LVGL-\test\MiddleWare\LVGL\GUI_APP\mainstart\mainstart.c
  * @Description: lvgl动画界面
  */
@@ -12,117 +12,88 @@
 #define WIDTH 240
 #define HEIGHT 320
 
-//三个obj全局变量初始化
-lv_obj_t* speed_label = NULL;
-lv_obj_t* speedUp_btn = NULL;
-lv_obj_t* speeddown_btn = NULL;
+//obj全局变量初始化
+static lv_obj_t* label1 = NULL;
+static lv_obj_t* label2 = NULL;
+static lv_obj_t* bar_obj = NULL;
 
-int speed = 0;
+static uint32_t val = 0;
 
 /**
- * @description: lvgl事件回调函数
+ * @description: 事件回调函数
  * @param {lv_event_t*} e
  * @return {void}
  */
-void Speed_btn_cb(lv_event_t* e)
+static void event_cb(lv_event_t* e)
 {
-    //判断obj
-    lv_obj_t* target = lv_event_get_target(e);
-    if (target == speedUp_btn)
-    {
-        speed += 30;
-    }
-    else if (target == speeddown_btn)
-    {
-        speed -= 30;
-    }
-    //最后限幅，也可以根据正负判断方向 
-    if (speed <= 0)
-    {
-        speed = 0;
-    }
-    lv_label_set_text_fmt(speed_label, "Speed: %d rpm", speed);
+
 }
 
 /**
- * @description: speed_label标签创建及其初始化
+ * @description: 定时器回调函数,在任务while(1)中被lv_timer_handler()调用,无需定义map等
+ * @return {*}
+ */
+void timer_cb(struct _lv_timer_t* User_timer)
+{
+    if (val < 100)
+    {
+        val++;
+        //更新显示(bar&label2)
+        //此处val不能被lv_bar_get_value替换，相同的数据替换不能触发动画
+        lv_bar_set_value(bar_obj, val, LV_ANIM_ON);
+        lv_label_set_text_fmt(label2, "%d  %%", lv_bar_get_value(bar_obj));
+    }
+    else
+    {
+        lv_label_set_text_fmt(label2, "Finished");
+
+    }
+}
+
+/**
+ * @description: 初始化label标签:LOADING和%d %%
  * @return {void}
  */
-static void Show_SpeedLabel(void)
+void label_example(void)
 {
-    //创建标签
-    speed_label = lv_label_create(lv_scr_act());
+    //创建LOADING和显示%
+    //创建LOADING并初始化
+    label1 = lv_label_create(lv_scr_act());
+    //对齐
+    lv_obj_align(label1, LV_ALIGN_CENTER, 0, -HEIGHT / 3);
     //文本
-    lv_label_set_text(speed_label, "Speed: 0 rpm");
-    //二参数见lv_conf.h里关于字体的裁剪宏
-    lv_obj_set_style_text_font(speed_label, &lv_font_montserrat_18, LV_STATE_DEFAULT);
-    //设置样式(本地设置)
-    lv_obj_set_style_bg_opa(speed_label, 200, LV_STATE_DEFAULT);    //更改不透明度
-    lv_obj_set_style_bg_color(speed_label, lv_color_hex(0x7160e8), LV_STATE_DEFAULT);   //设置颜色
+    lv_label_set_text(label1, "LOADING");
+
+    //创建%
+    label2 = lv_label_create(lv_scr_act());
     //对齐
-    lv_obj_align(speed_label, LV_ALIGN_CENTER, 0, -HEIGHT / 3);
+    lv_obj_align(label2, LV_ALIGN_CENTER, 0, HEIGHT / 3);
+    //文本
+    lv_label_set_text(label2, "  %");
+
 }
 
 /**
- * @description: speedup_btn obj创建及其初始化
+ * @description: 初始化进度条标签
  * @return {void}
  */
-static void Show_btn_SpeedUp()
+void bar_example(void)
 {
-    //创建按钮标签
-    speedUp_btn = lv_btn_create(lv_scr_act());
-    //设置样式
-    lv_obj_set_style_bg_color(speedUp_btn, lv_color_hex(0x4480ce), LV_STATE_DEFAULT);
+    //创建进度条
+    bar_obj = lv_bar_create(lv_scr_act());
     //对齐
-    lv_obj_align(speedUp_btn, LV_ALIGN_CENTER, -WIDTH / 3, 0);
-
-    //创建标签
-    static lv_obj_t* speedup_label = NULL;
-    speedup_label = lv_label_create(speedUp_btn);
-    //设置文本
-    lv_label_set_text(speedup_label,"Speed++");
-    //设置样式(略)
-    //对齐
-    lv_obj_align(speedup_label, LV_ALIGN_CENTER, 0, 0);
-
-    //绑定事件
-    lv_obj_add_event_cb(speedUp_btn, Speed_btn_cb, LV_EVENT_CLICKED, NULL);
-}
-
-/**
- * @description: speeddown_btn obj创建及其初始化
- * @return {void}
- */
-static void Show_btn_SpeedDown()
-{
-    //创建按钮标签
-    speeddown_btn = lv_btn_create(lv_scr_act());
-    //设置样式
-    lv_obj_set_style_bg_color(speeddown_btn, lv_color_hex(0x4480ce), LV_STATE_DEFAULT);
-    //对齐
-    lv_obj_align(speeddown_btn, LV_ALIGN_CENTER, WIDTH / 3, 0);
-
-    //创建标签
-    static lv_obj_t* speeddown_label = NULL;
-    speeddown_label = lv_label_create(speeddown_btn);
-    //设置文本
-    lv_label_set_text(speeddown_label, "Speed--");
-    //设置样式(略)
-    //对齐
-    lv_obj_align(speeddown_label, LV_ALIGN_CENTER, 0, 0);
-
-    //绑定事件
-    lv_obj_add_event_cb(speeddown_btn, Speed_btn_cb, LV_EVENT_CLICKED, NULL);
+    lv_obj_align(bar_obj, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_anim_time(bar_obj, 100, LV_STATE_DEFAULT);
+    //初始化定时器(100个节拍进入一次)
+    lv_timer_create(timer_cb, 100, NULL);
 
 }
-
 /**
  * @description: 主界面
  * @return {void}
  */
 void mainstart(void)
 {
-    Show_SpeedLabel();
-    Show_btn_SpeedUp();
-    Show_btn_SpeedDown();
+    label_example();
+    bar_example();
 }
